a000ac0177f263b85aa2421f464a735c1ae5ea56
Gustavo Rudolf <gustavorudolfifc@gmail.com>
2022-06-10 11:21:57 -0300

    salvo

---
+++ src/views/TimesView.vue
@@ -0,0 +1,110 @@
+<script>
+import { v4 as uuidv4 } from "uuid";
+export default {
+  data() {
+    return {
+      times: [
+        { id: "c9001f05-3c7b-447e-b1e7-83139d893025", nome: "Time 1" },
+        { id: "ff6923d3-6c9c-43fd-8c70-6ed292d1eda9", nome: "Time 2" },
+        { id: "fd9cd903-439e-4eb7-a2f2-7f8b4eaae4c4", nome: "Time 3" },
+      ],
+    };
+  },
+  methods: {
+    salvar() {
+      const novo_id = uuidv4();
+      this.times.push({
+        id: novo_id,
+        nome: this.novo_time,
+      });
+    },
+  },
+};
+</script>
+
+<template>
+  <div class="container">
+    <div class="title">
+      <h2>Gerenciamento de times</h2>
+    </div>
+    <div class="form-input">
+      <input type="text" v-model="novo_time" />
+      <button @click="salvar">Salvar</button>
+    </div>
+    <div class="list-times">
+      <table>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Nome</th>
+            <th>Ações</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="time in times" :key="time.id">
+            <td>{{ time.id }}</td>
+            <td>{{ time.nome }}</td>
+            <td>???</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </div>
+</template>
+<style>
+.title {
+  text-align: center;
+  margin: 2rem 0;
+}
+
+.form-input {
+  display: flex;
+  justify-content: center;
+  margin: 2rem 0;
+}
+
+.form-input input {
+  width: 50%;
+  padding: 0.5rem;
+  border: 1px solid #ccc;
+  border-radius: 10px;
+}
+
+.form-input button {
+  padding: 0.5rem;
+  width: 15%;
+  border: 1px solid rgb(20, 113, 20);
+  border-radius: 10px;
+  background-color: rgb(20, 113, 20);
+  color: white;
+  font-weight: bold;
+  margin-left: 1%;
+}
+
+.list-times {
+  display: flex;
+  justify-content: center;
+}
+
+table {
+  width: 50%;
+  border-collapse: collapse;
+  margin: 0 auto;
+  border: 1px solid black;
+  font-size: 1.1rem;
+  text-align: center;
+}
+
+table thead {
+  background-color: rgb(42, 24, 98);
+  color: white;
+}
+
+table thead th {
+  font-weight: bolder;
+}
+
+table tbody tr:nth-child(odd) {
+  background-color: rgb(189, 182, 182);
+}
+</style>
